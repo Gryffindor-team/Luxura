36b2dee51eae58e1b8dba417c6c214b0b6128587
abu-nofal <anofal719@gmail.com>
2021-05-24 09:44:03 +0300

    modfiy men

--- js/cart.js
+++ js/cart.js
@@ -1,34 +1,22 @@
 'use strict'
 
-if (localStorage.photo) {
-
-    let photoFromLS = JSON.parse(localStorage.photo);
-    
-    for (let i = 0; i < photoFromLS.length; i++) {
-        new MenImges(photoFromLS[i].name); //re-instantiation
-       
-
-        allProduct[i].renderImgMen();
+function displayCart (){
+    let cartItem =localStorage.getItem('prodectInCart');
+    cartItem=JSON.parse(cartItem);
+    let prodectContiner = document.getElementById('product');
+    if (cartItem && prodectContiner){
+        prodectContiner.innerHTML='';
+        Object.values(cartItem).map(item => {
+            prodectContiner.innerHTML +=`
+            <div class='product'>
+            <icon-icon name="close-circle"></icon-icon>
+                <img src= "../img/menImg/img1.jpg" >
+                <span>${item.name}</span>
+            </div>
+            `
+            
+        })
     }
 }
 
-// if (localStorage.clickcount) {
-
-//     let clickers = JSON.parse(localStorage.clickcount);
-    
-//     for (let i = 0; i < clickers.length; i++) {
-//         new MenImges(clickers[i].name); //re-instantiation
-       
-
-//         allProduct[i].renderImgMen();
-//     }
-// }
-
-
-
-let divForCart =document.getElementById('forCart');
-for(let i=0;i<allProduct.length;i++){
-    let p =document.createElement('p');
-    divForCart.appendChild(p);
-    p.innerText=allProduct[i].name
-}
+displayCart ();
